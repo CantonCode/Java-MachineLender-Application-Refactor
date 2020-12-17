b0edd95650b0542da5422da6e3794c85fc1ffbd4
SkepticQuark <quarkskeptic@gmail.com>
2020-12-17 09:40:55 +0000

    junit tests uncommented

--- src/sample/Test/EncryptionTest.java
+++ src/sample/Test/EncryptionTest.java
@@ -1,37 +1,37 @@
 package sample.Test;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import sample.Authentication.Model.Encryption;
 import sample.Authentication.Model.Decryption;
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /*
     EncryptionTest tests the functionality of encrypting and decrypting user and admin passwords for added security
     commented out due to not everyone's IDE having JUnit
  */
 class EncryptionTest {
-//    private Encryption encryptionTest = new Encryption();
-//    private Decryption decryptionTest = new Decryption();
-//   private final String passwordTest = "Test123";
-//    private final String expected = "Test123";
+    private Encryption encryptionTest = new Encryption();
+    private Decryption decryptionTest = new Decryption();
+   private final String passwordTest = "Test123";
+    private final String expected = "Test123";
 
     /*
         Tests if layer one encryption works as expected by encrypting and decrypting a test password
      */
-//    @Test
-//    public void passwordSecurityTest(){
-//        String encrypted = encryptionTest.layerOne(passwordTest);
-//        String actual = decryptionTest.layerOne(encrypted);
-//       assertEquals(expected, actual);
-//    }
+    @Test
+    public void passwordSecurityTest(){
+        String encrypted = encryptionTest.layerOne(passwordTest);
+        String actual = decryptionTest.layerOne(encrypted);
+       assertEquals(expected, actual);
+    }
 
     /*
         Tests if layer two encryption works as expected by encrypting and encrypting a test password
      */
-//    @Test
-//    public void passwordSecurityTest1(){
-//        String encrypted1 = encryptionTest.layerTwo(passwordTest);
-//        String actual1 = decryptionTest.layerTwo(encrypted1);
-//        assertEquals(expected, actual1);
-//    }
+    @Test
+    public void passwordSecurityTest1(){
+        String encrypted1 = encryptionTest.layerTwo(passwordTest);
+        String actual1 = decryptionTest.layerTwo(encrypted1);
+        assertEquals(expected, actual1);
+    }
 }
