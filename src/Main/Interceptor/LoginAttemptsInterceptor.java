7380c559298049043ce1dda31996cca0374d5169
Lodestoned <32818153+Lodestoned@users.noreply.github.com>
2021-04-14 15:12:29 +0100

    more interceptor updates

--- src/Main/Interceptor/LoginAttemptsInterceptor.java
+++ src/Main/Interceptor/LoginAttemptsInterceptor.java
@@ -1,18 +1,57 @@
 package Main.Interceptor;
 
 public class LoginAttemptsInterceptor implements IInterceptor{
+    private int attempts;
+
+    @Override
+    public void onProgramStart(IContextObject context) {
+        attempts = 0;
+    }
+
     @Override
     public void onPreLogin(IContextObject context) {
-
+        attempts = attempts + 1;
     }
 
     @Override
     public void onPostLogin(IContextObject context) {
+        System.out.printf("%d Login attempts.",attempts);
+    }
+
+    @Override
+    public void onPreCatalog(IContextObject context) {
 
     }
 
     @Override
-    public void meeeow(IContextObject context) {
+    public void onPostCatalog(IContextObject context) {
 
     }
+
+    @Override
+    public void onPreInventory(IContextObject context) {
+
+    }
+
+    @Override
+    public void onPostInventory(IContextObject context) {
+
+    }
+
+    @Override
+    public void onPreBorrow(IContextObject context) {
+
+    }
+
+    @Override
+    public void onPostBorrow(IContextObject context) {
+
+    }
+
+    @Override
+    public void onProgramExit(IContextObject context) {
+
+    }
+
+
 }
--- src/Main/Interceptor/TimeLoggingInterceptor.java
+++ src/Main/Interceptor/TimeLoggingInterceptor.java
@@ -3,19 +3,61 @@ package Main.Interceptor;
 public class TimeLoggingInterceptor implements IInterceptor {
 
     //// get post - pre and print. #time for functions.
+    private long start;
+
+    @Override
+    public void onProgramStart(IContextObject context) {
+
+    }
 
     @Override
     public void onPreLogin(IContextObject context) {
-        // if value !=null
+        start = System.currentTimeMillis();
     }
 
     @Override
     public void onPostLogin(IContextObject context) {
+        System.out.print("Time taken to Log In: ");
+        System.out.println(System.currentTimeMillis() - start);
+        start = 0;
+    }
+
+    @Override
+    public void onPreCatalog(IContextObject context) {
+        start = System.currentTimeMillis();
+    }
+
+    @Override
+    public void onPostCatalog(IContextObject context) {
+        System.out.print("Time taken Viewing Catalog: ");
+        System.out.println(System.currentTimeMillis() - start);
+        start = 0;
+    }
+
+    @Override
+    public void onPreInventory(IContextObject context) {
 
     }
 
     @Override
-    public void meeeow(IContextObject context) {
+    public void onPostInventory(IContextObject context) {
 
     }
+
+    @Override
+    public void onPreBorrow(IContextObject context) {
+
+    }
+
+    @Override
+    public void onPostBorrow(IContextObject context) {
+
+    }
+
+    @Override
+    public void onProgramExit(IContextObject context) {
+
+    }
+
+
 }
